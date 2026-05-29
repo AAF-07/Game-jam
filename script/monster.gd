fe953acdeac14851146f69dbc624a24058e79c9d
AAF-07 <firmansyahaziz38@gmail.com>
2026-05-29 16:16:52 +0700

    update movement monster

--- script/monster.gd
+++ script/monster.gd
@@ -1,34 +1,26 @@
 extends CharacterBody2D
 
-@export var SPEED = 150.0
-@export var is_chasing: bool = true
-
-var player: CharacterBody2D = null 
-
-func _ready() -> void:
-	# Mengambil objek pertama yang ada di dalam group "player"
-	var players = get_tree().get_nodes_in_group("player")
-	if players.size() > 0:
-		player = players[0] as CharacterBody2D
-	else:
-		print("PERINGATAN: Tidak ada objek di group 'player'. Pastikan Player sudah dimasukkan ke group!")
+@export var SPEED = 150
+var player_chase = false
+var player = null
 
 func _physics_process(delta: float) -> void:
-	# Validasi: Pastikan player ketemu dan mode mengejar aktif
-	if is_chasing and player != null:
-		# Hitung arah ke player (menghasilkan koordinat X dan Y)
-		var direction = position.direction_to(player.position)
+	if player_chase:
+		position += (player.position - position)/SPEED
 		
-		# Mengisi velocity untuk arah X dan Y sekaligus
-		velocity = direction * SPEED
-		
-		# Logika membalik sprite (Flip) berdasarkan arah X
-		if direction.x < 0:
-			$Sprite2D.flip_h = true  # Menghadap kiri
-		elif direction.x > 0:
-			$Sprite2D.flip_h = false # Menghadap kanan
-			
-		# PENTING: Fungsi ini yang menggerakkan monster di sumbu X dan Y
-		move_and_slide()
+		#buat nanti kalo ada animasi enemy
+		#if (position.player - position) < 0:
+			#flip_h = true
+		#else:
+			#flip_h = false
 	else:
-		velocity = Vector2.ZERO
+		pass #idle
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	player = null
+	player_chase = false
+	
