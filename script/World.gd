bfe548ea924260082d752f3ba44688fd2979f04a
AAF-07 <firmansyahaziz38@gmail.com>
2026-05-29 18:31:04 +0700

    movement

---
+++ script/World.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+#buat control UI Skill check dan sanity
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- script/monster.gd
+++ script/monster.gd
@@ -24,3 +24,10 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
 	
+func _on_hitbox_2_body_entered(body: Node2D) -> void:
+	if player_chase:
+		print("skill check")
+		
+		var world = get_tree().current_scene
+		if world.has_method("start_skill_check"):
+			world.start_skill_check()
